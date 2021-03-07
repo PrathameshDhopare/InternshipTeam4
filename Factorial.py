a7e7aa4e48d12d6ffec7d76b378a4f44b484bbd4
PrathameshDhopare <prathamdhopare1@gmail.com>
2021-03-07 12:53:39 +0530

    Factorial

---
+++ Factorial.py
@@ -0,0 +1,7 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num=int(input("enter number"))
+fact=1
+for i in range (1,num+1):
+    fact=fact*i
+print("Factorial=",fact)
